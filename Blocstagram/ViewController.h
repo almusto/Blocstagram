c7026e064f207ac64d916438e9e10ec5d214b88d
almusto <ales.musto@gmail.com>
2016-06-15 13:15:18 -0400

    Setting up

---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Alessandro Musto on 6/15/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
