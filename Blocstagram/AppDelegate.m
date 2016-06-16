e92569d9d41afb7ec2bdce956523e0e9137a15fd
almusto <ales.musto@gmail.com>
2016-06-16 14:37:19 -0400

    First Blocstagram Checkpoint

--- Blocstagram/AppDelegate.m
+++ Blocstagram/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ImagesTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,9 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    return YES;
-}
+        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        // Override point for customization after application launch.
+        self.window.backgroundColor = [UIColor whiteColor];
+        self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ImagesTableViewController alloc]init]];
+    
+        
+        [self.window makeKeyAndVisible];
+        return YES;
+    }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Blocstagram/ImagesTableViewController.h
+++ Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Alessandro Musto on 6/15/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ImagesTableViewController : UITableViewController
 
 @end
-
