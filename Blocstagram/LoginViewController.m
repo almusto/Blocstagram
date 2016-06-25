0c27bb98c78db93d042d5a164e1816aacaedbd3b
almusto <ales.musto@gmail.com>
2016-06-25 14:08:09 -0400

    Instagram Login Complete

--- Blocstagram/LoginViewController.m
+++ Blocstagram/LoginViewController.m
@@ -34,9 +34,10 @@ NSString *const LoginViewControllerDidGetAccessTokenNotification = @"LoginViewCo
     
     self.title = NSLocalizedString(@"Login", @"Login");
     
-    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@response_type=token", [DataSource instagramClientID], [self redirectURI]];
+    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [DataSource instagramClientID], [self redirectURI]];
     
     NSURL *url = [NSURL URLWithString:urlString];
+    
     
     if (url) {
         NSURLRequest *request = [NSURLRequest requestWithURL:url];
