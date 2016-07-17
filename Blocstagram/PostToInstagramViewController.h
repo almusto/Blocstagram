cfc937aca16fd090e67ddfbca9500aaffc05c26e
almusto <ales.musto@gmail.com>
2016-07-17 14:52:26 -0400

    Filter & Submit Checkpoint

---
+++ Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Alessandro Musto on 7/17/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
