651d5b79e8e5eb233d2ead90c1a640e1bd49efee
almusto <ales.musto@gmail.com>
2016-07-17 11:05:15 -0400

    Access Camera Checkpoint

---
+++ Blocstagram/CameraToolbar.h
@@ -0,0 +1,27 @@
+//
+//  CameraToolbar.h
+//  Blocstagram
+//
+//  Created by Alessandro Musto on 7/17/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CameraToolbar;
+
+@protocol CameraToolbarDelegate <NSObject>
+
+- (void) leftButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) rightButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+- (void) cameraButtonPressedOnToolbar:(CameraToolbar *)toolbar;
+
+@end
+
+@interface CameraToolbar : UIView
+
+- (instancetype) initWithImageNames:(NSArray *)imageNames;
+
+@property (nonatomic, weak) NSObject <CameraToolbarDelegate> *delegate;
+
+@end
---
+++ Blocstagram/UIImage+ImageUtilities.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+ImageUtilities.h
+//  Blocstagram
+//
+//  Created by Alessandro Musto on 7/17/16.
+//  Copyright © 2016 Lmusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ImageUtilities)
+
+- (UIImage *) imageWithFixedOrientation;
+- (UIImage *) imageResizedToMatchAspectRatioOfSize:(CGSize)size;
+- (UIImage *) imageCroppedToRect:(CGRect)cropRect;
+
+@end
+
